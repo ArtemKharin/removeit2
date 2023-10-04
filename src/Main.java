138247ce8b4b5ae1d30e968c729c732f3d352ad7
Artem Kharin <artem.kharin96@gmail.com>
2023-10-04 19:33:32 +0300

    fix error in hw2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int anotherNumber = 5_000;
         short smallNumber = (short) anotherNumber;
-        System.out.println(smallNumber);
         System.out.println(anotherNumber);
     }
 }
